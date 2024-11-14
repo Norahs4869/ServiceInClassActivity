d759aa3e82f06d1b97b24ff7597ce23e499ec7c2
Norahs4869 <tul29737@temple.edu>
2024-11-14 15:01:43 -0500

    added the start and pause button functionality

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,68 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+    val serviceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val startButton = findViewById<Button>(R.id.startButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
+
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+            if(timerBinder.isRunning == false){
+                if(isConnected){
+                    startButton.text = "Pause"
+                    timerBinder.start(100)
+
+                }
+            } else{
+                if(isConnected){
+                    startButton.text = "Start"
+                    timerBinder.pause()
+                }
+            }
+
 
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
 
         }
+    }
+
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
     }
 }
