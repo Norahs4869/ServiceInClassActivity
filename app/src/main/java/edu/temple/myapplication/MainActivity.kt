5988094b33a700c29633fafcf29b483e75287aea
Norahs4869 <tul29737@temple.edu>
2024-11-14 15:12:47 -0500

    Added the handler so now the countdown is on the textview

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,18 +5,29 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerTextView: TextView
+
     lateinit var timerBinder: TimerService.TimerBinder
     var isConnected = false
+
+    val timerHandler = Handler(Looper.getMainLooper()){
+        timerTextView.text = it.what.toString()
+        true
+    }
 
     val serviceConnection = object: ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             timerBinder = service as TimerService.TimerBinder
+            timerBinder.setHandler(timerHandler)
             isConnected = true
         }
 
@@ -32,6 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         val startButton = findViewById<Button>(R.id.startButton)
         val stopButton = findViewById<Button>(R.id.stopButton)
+        timerTextView = findViewById(R.id.textView)
 
         bindService(
             Intent(this, TimerService::class.java),
